b7d0e3931e73f67ee89e6f360a2cb14b4375d80a
Dmytro <ipz223_kdm@student.ztu.edu.ua>
2024-05-05 17:43:18 +0300

    Added basic implementation of command, rewritten examples using commands.

---
+++ Lab03/PatternExamples/Composite/Commands/DemonstrateStrategy.cs
@@ -0,0 +1,33 @@
+﻿using Composite.Command;
+using Composite.Core;
+using Composite.Core.Elements.Image;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Commands
+{
+    public class DemonstrateStrategy : LightHTMLCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            var root = new LightElementNode("body");
+
+            root.Children.Add(new ImageNode("https://google.com/somepicture1"));
+            root.Children.Add(new ImageNode("folder1/happy.png", new FileImageSource()));
+
+
+            void DisplayLightNode(LightNode lightNode)
+            {
+                foreach (var line in lightNode.GetLazyOuterHTML())
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            DisplayLightNode(root);
+        }
+    }
+}
